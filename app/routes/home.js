99100f639822eaf9244427fed6b77406c83ddd43
TheMissenoOne <39657810+TheMissenoOne@users.noreply.github.com>
2020-04-16 13:37:42 -0300

    Update home.js

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,9 +1,5 @@
- 
-const Item = require('../models/Item');
 module.exports = function(express){
     express.get('/', function(req, res){
-                Item.find()
-          .then(items => res.render('home/index"', { items }))
-          .catch(err => res.status(404).json({ msg: 'No items found' }));
+                res.render('home/index");
     });
 };
